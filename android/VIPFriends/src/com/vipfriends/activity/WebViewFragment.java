d2849e3a509b81e6a7f46648a7276baa263e53c0
keshuangjie <keshuangjie@gmail.com>
2013-12-04 15:17:27 +0800

    重新构建BottomBar

---
+++ android/VIPFriends/src/com/vipfriends/activity/WebViewFragment.java
@@ -0,0 +1,152 @@
+package com.vipfriends.activity;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.vipfriends.util.DLog;
+import com.vipfriends.util.UserPhotoUpload;
+import com.vipfriends.view.ProgressWebView;
+
+/**
+ * 
+ * @author keshuangjie
+ */
+public class WebViewFragment extends BaseFragment {
+	private static final String TAG = "WebViewFragment";
+	
+	public static final String JS_INVOKE = "invoke";
+	
+	//本地图片
+	private final static int LOCAL_PICTURE = 1;
+	//照相图片
+	private final static int CAMERA_PICTURE = 2;
+	//剪裁图片
+	private final static int CUT_PICTURE = 3;
+
+	private ProgressWebView mWebView;
+	private String mUrl;
+	
+	private UserPhotoUpload mUserPhotoUpload;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		DLog.i(TAG, "onCreate");
+		Bundle arguments = getArguments();
+		if(arguments != null){
+			mUrl = arguments.getString("url");
+		}else{
+			DLog.i(TAG, "arguments is null");
+		}
+		DLog.i(TAG, "url=" + mUrl);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		DLog.i(TAG, "onCreate");
+		
+		if(mWebView == null){
+			
+			DLog.i(TAG, "mWebView == null");
+			
+			mWebView = new ProgressWebView(getActivity());
+
+			mWebView.setWebViewClient(new WebViewClient() {
+				public void onReceivedError(WebView view, int errorCode,
+						String description, String failingUrl) {
+					Toast.makeText(getActivity(), "Oh no! " + description,
+							Toast.LENGTH_SHORT).show();
+				}
+				public boolean shouldOverrideUrlLoading(WebView view, String url) {
+					view.loadUrl(url);
+					return true;
+				}
+			});
+			mWebView.addJavascriptInterface(new JSInvoke(), JS_INVOKE);
+			
+//			mWebView.loadUrl("file:///android_asset/register.html");
+			mWebView.loadUrl(mUrl);
+		}
+		
+		ViewGroup parent = (ViewGroup) mWebView.getParent(); 
+		if (parent != null) {            
+			parent.removeView(mWebView);      
+		} 
+//		mWebView.requestFocus();
+//		mWebView.setOnTouchListener(new View.OnTouchListener() {
+//			@Override
+//			public boolean onTouch(View v, MotionEvent event) {
+//				v.requestFocusFromTouch();
+//				return false;
+//			}
+//		});
+		return mWebView;
+	}
+	
+	public boolean canGoBack(){
+		DLog.i(TAG, "mWebView.canGoBack():" + mWebView.canGoBack());
+		if(mWebView.canGoBack()){
+			mWebView.goBack();
+		}
+		return mWebView.canGoBack();
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			switch (requestCode) {
+			// 如果是直接从相册获取
+			case LOCAL_PICTURE:
+				if (data != null && mUserPhotoUpload!=null) {
+					mUserPhotoUpload.startPhotoZoom(data.getData());
+				}
+				break;
+			// 如果是调用相机拍照时
+			case CAMERA_PICTURE:
+				try {
+					File userPhotoFile = mUserPhotoUpload.getUserPhotoFile();
+					if (userPhotoFile != null && userPhotoFile.isFile() && mUserPhotoUpload!=null) {
+						mUserPhotoUpload.startPhotoZoom(Uri.fromFile(userPhotoFile));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			// 取得裁剪后的图片
+			case CUT_PICTURE:
+				if (data != null && mUserPhotoUpload!=null) {
+					mUserPhotoUpload.setPicToView(data);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	private class JSInvoke{
+		
+		@SuppressWarnings("unused")
+		public void updateUserFace(){
+			DLog.i(TAG, "updateUserFace():js invoke java success");
+			mUserPhotoUpload = new UserPhotoUpload(getActivity());
+			mUserPhotoUpload.ShowPickDialog();
+		}
+		
+	}
+	
+}
--- android/VIPFriends/src/com/vipfriends/view/ProgressWebView.java
+++ android/VIPFriends/src/com/vipfriends/view/ProgressWebView.java
@@ -1,14 +1,20 @@
 package com.vipfriends.view;
 
+import com.vipfriends.util.DLog;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
+import android.webkit.JsResult;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class ProgressWebView extends WebView {
-
+	private static final String TAG = "ProgressWebView";
+	
 	private ProgressBar mProgressbar;
 	private Context mContext;
 
@@ -19,22 +25,33 @@ public class ProgressWebView extends WebView {
 	}
 
 	public ProgressWebView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        this.mContext = context;
-        init();
-    }
+		super(context, attrs);
+		this.mContext = context;
+		init();
+	}
 
 	@SuppressLint("SetJavaScriptEnabled")
 	private void init() {
 		mProgressbar = new ProgressBar(mContext, null,
 				android.R.attr.progressBarStyleHorizontal);
 		mProgressbar.setLayoutParams(new ViewGroup.LayoutParams(
-				ViewGroup.LayoutParams.MATCH_PARENT, 3));
+				ViewGroup.LayoutParams.MATCH_PARENT, 6));
 		addView(mProgressbar);
-		
+
 		this.getSettings().setJavaScriptEnabled(true);
-		this.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+		this.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+		
+		/** scrollBar样式 */
+		this.setScrollbarFadingEnabled(true);
+		this.setVerticalScrollBarEnabled(false);
+		this.setOverScrollMode(WebView.OVER_SCROLL_ALWAYS);
+		
 		this.setWebChromeClient(new WebChromeClient());
+	}
+	
+	@Override
+	public boolean onCheckIsTextEditor() {
+		return true; 
 	}
 
 	public class WebChromeClient extends android.webkit.WebChromeClient {
@@ -50,6 +67,20 @@ public class ProgressWebView extends WebView {
 			super.onProgressChanged(view, newProgress);
 		}
 
+		@Override
+		public boolean onJsAlert(WebView view, String url, String message,
+				JsResult result) {
+			DLog.i(TAG, "Alert message: " + message);
+			final String alertMessage = message;
+			if (!TextUtils.isEmpty(message)) {
+				Toast.makeText(mContext, alertMessage, Toast.LENGTH_LONG)
+						.show();
+				result.cancel();
+				return true;
+			}
+			return super.onJsAlert(view, url, message, result);
+		}
+
 	}
 
 	@SuppressWarnings("deprecation")
@@ -61,4 +92,5 @@ public class ProgressWebView extends WebView {
 		mProgressbar.setLayoutParams(lp);
 		super.onScrollChanged(l, t, oldl, oldt);
 	}
+	
 }
